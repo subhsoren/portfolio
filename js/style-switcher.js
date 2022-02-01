a03b7b2688f730a2ab14da2df8d283577f26e3fe
subhash soren <subhashsoren44@gmail.com>
2022-02-01 15:42:14 +0530

    theme color changes section done

---
+++ js/style-switcher.js
@@ -0,0 +1,27 @@
+// toggle style switcher
+const styleSwitcherToggle = document.querySelector('.style-switcher-toggler');
+styleSwitcherToggle.addEventListener('click', ()=> {
+    document.querySelector('.style-switcher').classList.toggle('open');
+})
+
+// hide style switcher
+window.addEventListener("scroll", () => {
+    if(document.querySelector('.style-switcher').classList.contains('open')) {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+
+})
+
+//theme colors
+const alternateStyles = document.querySelectorAll('.alternate-style');
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute('title')) 
+        {
+            style.removeAttribute('disabled');
+        }
+         else {
+            style.setAttribute('disabled', 'true');
+        }
+    })
+}

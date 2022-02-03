b91102e3e8cc72865ce06c455a45c7efdeb965ee
subhash soren <subhashsoren44@gmail.com>
2022-02-03 18:07:50 +0530

    aside section changes

--- js/main.js
+++ js/main.js
@@ -5,3 +5,24 @@ var typed = new Typed(".typing", {
     backSpeed: 60,
     loop: true,
 });
+
+// Aside section
+const nav = document.querySelector('.nav'),
+   navList = nav.querySelectorAll("li"),
+   totalNavList = navList.length;
+   for(let i =0; i < totalNavList; i++) {
+    //    console.log(navList[i]);
+    const a = navList[i].querySelector("a");
+    // console.log(a);
+    a.addEventListener('click', () => {
+        // console.log(this);
+        for (let j=0; j<totalNavList; j++) {
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        this.classList.add("active")
+        showSection(this);
+    })
+   }
+   function showSection(element) {
+       console.log(element);
+   }

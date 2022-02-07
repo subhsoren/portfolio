778ac6cef2c99caf705709ca3ccbd9b0f0ef2201
subhash soren <subhashsoren44@gmail.com>
2022-02-07 12:19:07 +0530

    close button added in humburger icon

--- js/main.js
+++ js/main.js
@@ -47,6 +47,10 @@ const nav = document.querySelector('.nav'),
 
    function asideSectionTogglerBtn() {
        aside.classList.toggle("open");
-       
+       navTogglerBtn.classList.toggle("open");
+       for(let i=0; i<totalSection; i++) {
+           allSection[i].classList.toggle("open");
+       }
+
 
    }
